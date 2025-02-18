0521094c78e804eca8d33c08e3afcdf3c8ffe0f3
Juciano de Carvalho Barbosa <juciano.barbosa@sensedia.com>
2025-02-18 12:51:23 -0300

    init

--- packages/tailwind-config/tailwind.config.cjs
+++ packages/tailwind-config/tailwind.config.cjs
@@ -2,7 +2,65 @@
 module.exports = {
   darkMode: ['class'],
   theme: {
-    extend: {},
+    extend: {
+      typography: {
+        base: {
+          css: {
+            color: 'var(--neutral-base)',
+            a: {
+              color: 'var(--primary-base)',
+              '&:hover': {
+                color: 'var(--primary-strong)',
+              },
+            },
+          },
+        }
+      },
+      colors:{
+        primary: {
+          softer: 'var(--primary-softer)',
+          soft: 'var(--primary-soft)',
+          base: 'var(--primary-base)',
+          strong: 'var(--primary-strong)',
+          stronger: 'var(--primary-stronger)',
+        },
+        neutral:{
+          softest: 'var(--neutral-softest)',
+          softer: 'var(--neutral-softer)',
+          soft: 'var(--neutral-soft)',
+          base: 'var(--neutral-base)',
+          strong: 'var(--neutral-strong)',
+          stronger: 'var(--neutral-stronger)',
+          strongest: 'var(--neutral-strongest)',    
+        },
+
+        destructive: {
+          softer: 'var(--destructive-softer)',
+          soft: 'var(--destructive-soft)',
+          base: 'var(--destructive-base)',
+        },
+
+        success: {
+          soft: 'var(--success-soft)',
+          base: 'var(--success-base)',
+        },
+
+        warning: {
+          soft: 'var(--warning-soft)',
+          base: 'var(--warning-base)',
+        },
+
+        info: {
+          soft: 'var(--info-soft)',
+          base: 'var(--info-base)',
+        },
+
+        error:{
+          soft: 'var(--error-soft)',
+          base: 'var(--error-base)',
+        }
+      },
+    },
   },
   plugins: [
     require('tailwindcss-animate'),
